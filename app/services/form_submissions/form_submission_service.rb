9d5f9a08b453849f1ad50ca7fbdbe530036f317e
mauriciob525 <mauricio.b@cincoventicinco.com>
2026-01-16 14:11:50 -0500

    fix: update condition to check for blank values in form submissions

--- app/services/form_submissions/form_submission_service.rb
+++ app/services/form_submissions/form_submission_service.rb
@@ -5,7 +5,7 @@ module FormSubmissions
     
     def update_submission(form_submission, params)
       submissions = params[:form_submission_values_attributes]
-      submissions.each { |submission| submission[:_destroy] = true if submission[:value].empty? }
+      submissions.each { |submission| submission[:_destroy] = true if submission[:value].blank? }
       form_submission.update(params)
     end
   end
