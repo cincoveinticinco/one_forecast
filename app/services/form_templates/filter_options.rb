f33acff5ce7abded2300494bbf5bf7e4eaa8ba23
mauriciob525 <mauricio.b@cincoventicinco.com>
2026-01-19 17:08:37 -0500

    refactor: simplify filter options by using distinct pluck instead of enum mapping

--- app/services/form_templates/filter_options.rb
+++ app/services/form_templates/filter_options.rb
@@ -2,31 +2,19 @@ module FormTemplates
   class FilterOptions
     def self.call
       {
-        status: enum_options(FormTemplate.statuses),
-        template_type: enum_options(FormTemplate.template_types),
-        access_type: enum_options(FormTemplate.access_types),
+        status: FormTemplate.distinct.pluck(:status),
+        template_type: FormTemplate.distinct.pluck(:template_type),
+        access_type: FormTemplate.distinct.pluck(:access_type),
         order: order_options
       }
     end
 
     private
 
-    def self.enum_options(enum)
-      enum.keys.map do |key|
-        {
-          value: key,
-          label: key.humanize
-        }
-      end
-    end
-
     def self.order_options
       FilterQuery::ALLOWED_ORDER_COLUMNS.flat_map do |column|
         [
-          { 
-            value: "#{column}", 
-            label: "#{column.humanize}" 
-          }
+          "#{column}",
         ]
       end
     end
