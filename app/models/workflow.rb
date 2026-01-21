e64356f4c648ffc9bf6a7aa9bd6d813804de96fe
alexandero525 <alexander.ospina@cincoveinticinco.com>
2026-01-21 11:11:24 -0500

    .

--- app/models/workflow.rb
+++ app/models/workflow.rb
@@ -1,3 +1,14 @@
 class Workflow < ApplicationRecord
   belongs_to :tenant
+  has_many :form_templates
+
+  before_destroy :ensure_not_associated_to_forms
+  private
+  def ensure_not_associated_to_forms
+    if form_templates.exists?
+      errors.add(:base, 'No se puede eliminar el workflow porque está asociado a formularios')
+      throw :abort
+    end
+  end
+
 end
