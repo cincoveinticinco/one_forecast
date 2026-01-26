b40cc3fa8e0bc27874f0ca21ee31d9a9e53dc4eb
206668528_nbcuni <DavidMauricio.Bernal@nbcuni.com>
2026-01-26 12:12:12 -0500

    feat: enhance search scope to support multiple submission terms

--- app/models/form_submission.rb
+++ app/models/form_submission.rb
@@ -8,9 +8,19 @@ class FormSubmission < ApplicationRecord
 
   after_initialize :set_default_status, if: :new_record?
 
-  scope :search_by_submission, ->(search_term) {
+  scope :search_by_submission, ->(search_terms) {
+    return all if search_terms.blank?
+
+    terms = search_terms.is_a?(String) ? search_terms.split(/\s+/) : Array(search_terms)
+    terms = terms.map(&:strip).reject(&:blank?)
+
+    return all if terms.empty?
+
+    conditions = terms.map { "form_submission_values.value LIKE ?" }
+    values = terms.map { |term| "%#{sanitize_sql_like(term)}%" }
+
     joins(:form_submission_values)
-      .where("form_submission_values.value LIKE ?", "%#{sanitize_sql_like(search_term)}%")
+      .where(conditions.join(" OR "), *values)
       .distinct
   }
 
